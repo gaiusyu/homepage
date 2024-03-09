5bb4c4115504142303a233dfe1bbae40799adc39
PaperReviewww <3111765384@qq.com>
2024-03-09 16:49:10 +0800

    Updates

---
+++ p__Award__index.async.js
@@ -0,0 +1,33 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[317],{65273:function(l,i,n){n.d(i,{Z:function(){return g}});var e=n(62435),o=n(46837),t=n(8250),d=n(29671),r=n(20640),c=n.n(r),h=n(33862),v=n(1210),m=n(85093),x=n(10524),j=n(14079),a=n(49721),u=n.p+"static/logo.716e211f.png",f=`**RA** \r
+in CUHKSZ.\r
+\r
+`,s=n(86074),g=function(){var N=function(S){S.preventDefault(),c()(a.email),o.ZP.success("Email copied to clipboard",1)};return(0,s.jsxs)("div",{className:"sidebar",children:[(0,s.jsxs)("div",{className:"profile",children:[(0,s.jsx)(t.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:u}),(0,s.jsxs)("div",{className:"info",children:[(0,s.jsx)("div",{className:"name",children:a.name}),(0,s.jsx)("div",{className:"shortInfo",children:(0,s.jsx)(d.U,{children:f})})]})]}),(0,s.jsxs)("div",{className:"link",children:[(0,s.jsxs)("div",{children:[(0,s.jsx)(h.Z,{className:"icon"}),a.location]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(v.Z,{className:"icon"}),(0,s.jsx)("a",{href:a.github,children:"Github"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(m.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.twitter,children:"Twitter"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(x.Z,{className:"icon"}),(0,s.jsx)("a",{onClick:N,href:"",children:"Email"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(j.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.googleScholar,children:"Google Scholar"})]})]})]})}},66920:function(l,i,n){n.r(i),n.d(i,{default:function(){return c}});var e=n(62435),o=`# \u5927\u5BB6\u597D\uFF0C\u8FD9\u662F\u6D4B\u8BD5\u7248\u7684Markdown\u6587\u6863\r
+\r
+\u8FD9\u662F\u4E00\u6BB5\u793A\u4F8B\u6587\u672C\uFF0C\u4EE5\u4E0B\u662F\u4E00\u4E9BMarkdown\u7684\u57FA\u672C\u8BED\u6CD5\u793A\u4F8B\u3002\r
+\r
+## \u5C0F\u6807\u9898 1\r
+\r
+- \u5217\u8868\u98791\r
+- \u5217\u8868\u98792\r
+- \u5217\u8868\u98793\r
+\r
+### \u5C0F\u5C0F\u6807\u9898 1\r
+\r
+\u8FD9\u662F\u4E00\u6BB5*\u659C\u4F53*\u6587\u672C\uFF0C\u8FD9\u662F\u4E00\u6BB5**\u7C97\u4F53**\u6587\u672C\u3002\r
+\r
+#### \u5C0F\u5C0F\u5C0F\u6807\u9898 1\r
+\r
+1. \u6709\u5E8F\u5217\u8868\u98791\r
+2. \u6709\u5E8F\u5217\u8868\u98792\r
+3. \u6709\u5E8F\u5217\u8868\u98793\r
+\r
+##### \u5C0F\u5C0F\u5C0F\u5C0F\u6807\u9898 1\r
+\r
+> \u8FD9\u662F\u4E00\u4E2A\u5F15\u7528\u3002\r
+\r
+###### \u6700\u5C0F\u6807\u9898\r
+\r
+\`\`\`python\r
+# \u8FD9\u662F\u4E00\u6BB5\u4EE3\u7801\r
+print("Hello, Markdown!")\r
+`,t=n(65273),d=n(29671),r=n(86074),c=function(){return(0,r.jsxs)("div",{className:"page",children:[(0,r.jsx)(t.Z,{}),(0,r.jsxs)("div",{className:"content",children:[(0,r.jsx)("div",{className:"title",children:"Awards"}),(0,r.jsx)("div",{className:"markdown",children:(0,r.jsx)(d.U,{children:o})})]})]})}},49721:function(l){l.exports=JSON.parse('{"name":"Siyu Yu","location":"ShenZhen, China","email":"gaiusyu6@gmail.com","github":"https://github.com/gaiusyu","twitter":"https://twitter.com/None","googleScholar":"https://scholar.google.com/citations?user=PBRCSToAAAAJ&hl=en&oi=sra","showBar":["aboutme","publications","awards","services","cv"]}')}}]);
---
+++ p__Services__index.async.js
@@ -0,0 +1,14 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[767],{65273:function(e,l,n){n.d(l,{Z:function(){return g}});var d=n(62435),o=n(46837),t=n(8250),r=n(29671),i=n(20640),c=n.n(i),v=n(33862),h=n(1210),m=n(85093),x=n(10524),j=n(14079),a=n(49721),u=n.p+"static/logo.716e211f.png",f=`**RA** \r
+in CUHKSZ.\r
+\r
+`,s=n(86074),g=function(){var N=function(S){S.preventDefault(),c()(a.email),o.ZP.success("Email copied to clipboard",1)};return(0,s.jsxs)("div",{className:"sidebar",children:[(0,s.jsxs)("div",{className:"profile",children:[(0,s.jsx)(t.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:u}),(0,s.jsxs)("div",{className:"info",children:[(0,s.jsx)("div",{className:"name",children:a.name}),(0,s.jsx)("div",{className:"shortInfo",children:(0,s.jsx)(r.U,{children:f})})]})]}),(0,s.jsxs)("div",{className:"link",children:[(0,s.jsxs)("div",{children:[(0,s.jsx)(v.Z,{className:"icon"}),a.location]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(h.Z,{className:"icon"}),(0,s.jsx)("a",{href:a.github,children:"Github"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(m.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.twitter,children:"Twitter"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(x.Z,{className:"icon"}),(0,s.jsx)("a",{onClick:N,href:"",children:"Email"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(j.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.googleScholar,children:"Google Scholar"})]})]})]})}},92244:function(e,l,n){n.r(l),n.d(l,{default:function(){return c}});var d=n(62435),o=`# Service\r
+\r
+\r
+## External Reviewer \u2014\u2014 2023\r
+\r
+- IEEE International Conference onSoftware Analysis, Evolution and Reengineering (SANER 2023)\r
+- Empirical Software Engineering (EMSE 2023) (CCF-B)\r
+- \u5217\u8868\u98793\r
+\r
+\r
+`,t=n(65273),r=n(29671),i=n(86074),c=function(){return(0,i.jsxs)("div",{className:"page",children:[(0,i.jsx)(t.Z,{}),(0,i.jsxs)("div",{className:"content",children:[(0,i.jsx)("div",{className:"title",children:"Services"}),(0,i.jsx)("div",{className:"markdown",children:(0,i.jsx)(r.U,{children:o})})]})]})}},49721:function(e){e.exports=JSON.parse('{"name":"Siyu Yu","location":"ShenZhen, China","email":"gaiusyu6@gmail.com","github":"https://github.com/gaiusyu","twitter":"https://twitter.com/None","googleScholar":"https://scholar.google.com/citations?user=PBRCSToAAAAJ&hl=en&oi=sra","showBar":["aboutme","publications","awards","services","cv"]}')}}]);
